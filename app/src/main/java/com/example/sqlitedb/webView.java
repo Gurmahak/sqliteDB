edeb86878763b040330f48176627f99125399443
Gurmahak <gurmahak94@gmail.com>
2019-11-06 08:31:58 -0800

    Initial commit

---
+++ app/src/main/java/com/example/sqlitedb/webView.java
@@ -0,0 +1,27 @@
+package com.example.sqlitedb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class webView extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_web_view);
+
+        WebView webView = findViewById(R.id.webView);
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.setWebViewClient(new WebViewClient());
+
+        Intent i = getIntent();
+        String link = i.getStringExtra("Link");
+        Log.d("Link Passed is : ", link);
+        webView.loadUrl("https" + link);
+    }
+}
